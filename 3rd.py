48ff2ff5af1f718077e7e00ef47beccec431a8a5
“[firstname <binty.sherry@gmail.com>
2024-11-03 23:06:04 +0500

    six commit

---
+++ 3rd.py
@@ -0,0 +1,120 @@
+
+
+
+
+# loops   loops i table py chlty hen.....
+# for loop
+
+name = "sherry"  
+
+for character in name:
+    print(character)
+
+number = [1,2,3,4,5,6,7,8]
+for character in number:
+    print(character)  #simple loop
+
+    # loop range
+
+for x in range(1,10):
+    print("Atif")
+
+for x in range(1,10,2):
+    print(x)
+
+#for x in range(1,101):
+  #  print(x)
+
+s = "geeks"
+for i in s:
+    print(i)
+
+for i in range(0,10,2):
+ print(i)
+
+# enumerate
+
+name = "iqra "
+for char in name:
+    print(char)
+
+for index , char in enumerate(name):
+    print(index,":", char)
+
+# nested for loop
+
+
+for x in range(1,4):
+    for y in range(1,4):
+        print(x,y)
+
+
+# while loop
+
+
+count = 0
+while (count < 3):
+    print("hello sherry")
+    count = count + 1  # count += 1    ye count ko false krna zroori h.
+
+
+
+# control statements
+
+
+for x in range(1,10):
+    if x == 5:     # 5 skip ho jay ga
+        continue
+    print(x)
+
+for x in range(1,10):
+    if x == 3:     # 3 skip ho jay ga
+        continue
+    print(x)
+
+#break statement
+
+for x in range(1,10):
+    if x == 3:    
+        break
+    print(x)
+
+
+
+#Assignment
+for x in range(7,77,7):
+    print (x)
+
+# table assignment
+
+user_input=int(input())
+for x in range (1,11):
+     print(f"{user_input} x {x} = {user_input*x}")
+
+
+# * print
+for x in range(1,4):
+    print("*****")
+
+
+for x in range(1,101):
+    
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    else:
+        print(x)
+
+
+
+
+
+
+
+    
+
+
+
